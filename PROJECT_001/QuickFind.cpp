1840f6b22fc7f60d81e86ad7494da2f0ef79cf32
omid111621 <omid111621@gmail.com>
2022-05-01 21:58:03 +0430

    third commit

--- PROJECT_001/QuickFind.cpp
+++ PROJECT_001/QuickFind.cpp
@@ -2,9 +2,9 @@
 //Quick-find solution to connectivity problem
 
 //input......................
-// (p, q) pairs
+// N and (p, q) pairs
 //functionality..............
-//1. produce an array of N elements.(explaination: N > p , q)
+//1. produce an array of N elements and print it.(explaination: N > p , q)
 //2. Check whether p and q are connected.
 //(explaination: p and q are connected if and only if refrenceArray[p]==refrenceArray[q])
 //3. if (related) do {nothing}.
@@ -25,18 +25,79 @@
 */
 
 #include <iostream>
+#include <iterator> //std::ostream_iterator
+#include <algorithm> //std::copy
 using namespace std;
-int main() {
-    cout << "Enter p then q" << endl;
-    const int n = 15;
-    int p, q, referenceArray[n];
-    cin >> p >> q ;
-    //1. produce an array of N elements.
-    for(int i = 0; i <= n; i++) referenceArray[i] = i;
-    cout << referenceArray[3] << endl;
 
+
+int main() {
+    cout << "Enter the value for N" << endl;
+    int n;
+    cin >> n;
+    const int N = n; // It is a good practice to give "those parameters that we do not want to change" a type of "const".
+    int p, q, referenceArray[N];
+
+    // produce an array of N elements.....................
+    for(int i = 0; i < N; i++) referenceArray[i] = i;
+    //print referenceArray................................
+    cout << "You referenceArray is :" << endl;
+    ostream_iterator <int> out_it (cout, ",");
+    copy (referenceArray, referenceArray + N, out_it);
+    cout << endl;
+    //Ask user for p and q pairs..........................
+    cout << "Please enter your pair p q" << endl;
+
+    //Main program begins here!...........................
+     while (cin >> p >> q) { 
+         
+          
+         if ( referenceArray[p] == referenceArray[q]) {
+            cout << "p , q are  related" << endl;
+            copy (referenceArray, referenceArray + N, out_it);
+            cout << endl;
+             continue;
+             }
+            int memory = referenceArray [p];// Tricky point. Notice to the next comment.
+            for(int i = 0; i < N; i++){
+            //int memory = referenceArray [p]; // There is a tricky point here! we should not define our "memory" here.
+                                               // The reason is that, although in the "for loop" the value of "p" do not change.
+                                               // But, the value of "referenceArray[p]" is very likely to change then "memory"
+                                               // will change, and that cuase bugs. 
+                                               // To illustrate, the valuse of referenceArray[p] may change because the valuse of 
+                                               // referenceArray[i] is changing incrimently, and at some point the value of "i" happens 
+                                               // to be similar to the value of "p" !!!!
+                                               // So, be extra careful.
+                if ( referenceArray[i] == memory) referenceArray [i] = referenceArray [q];
+            }
+            cout << "Dear User,\n"<< p << "and" << q << "are not related!\nBut do not worry! I have just made them related!\nYour referenceArray has changed into :" << endl;
+            //print referenceArray............................
+            copy (referenceArray, referenceArray + N, out_it);
+            cout << endl;
+            //Ask user for new input..........................
+            cout << "Please enter another pair" << endl;
+             
+     }
+   
     return 0;
 }
+
+// Here, you can find a simpler version of the program.
+
+// #include <iostream.> 
+// using namespace std;
+// static const int N = 10;
+// int main (){
+//     int i, p, q, id[N];
+//     for (i=0; i < N; i++) id[i] = i;
+//     while (cin >> p >> q) {
+//         int t = id[p];
+//         if (t == id[q]) continue;
+//         for (i=0; i < N; i++)
+//             if(id [i] == t) id[i] = id[q];
+//         cout << "" << p << "" << q << endl;
+//          
+//     }
+// }
 
 
 
